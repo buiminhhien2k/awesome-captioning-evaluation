ef4f63e35fb579aeba7cd2ab662c6736433c522a
Hien Bui <buiminhhien2k@gmail.com>
2026-05-23 00:31:41 +1000

    add requires_references property to BaseMetric

--- metrics/base_metric.py
+++ metrics/base_metric.py
@@ -1,10 +1,19 @@
 from abc import ABC, abstractmethod
 
+
 class BaseMetric(ABC):
+    @property
+    @abstractmethod
+    def requires_references(self) -> bool:
+        """
+        Whether this metric requires reference captions.
+        """
+        pass
+
     @abstractmethod
     def load_model(self, **kwargs):
         pass
-    
+
     @abstractmethod
     def compute_score(self, *args, **kwargs):
         pass
