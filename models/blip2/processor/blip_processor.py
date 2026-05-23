ff0fe7193d905ea75b1688be28898d72dd066d98
Hien Bui <buiminhhien2k@gmail.com>
2026-05-23 14:43:54 +1000

    remove cv2 from blip2

--- models/blip2/processor/blip_processor.py
+++ models/blip2/processor/blip_processor.py
@@ -8,7 +8,7 @@
 import re
 
 from models.blip2.processor.base_processor import BaseProcessor
-from models.blip2.processor.randaugment import RandomAugment
+# from models.blip2.processor.randaugment import RandomAugment
 from omegaconf import OmegaConf
 from torchvision import transforms
 from torchvision.transforms.functional import InterpolationMode
@@ -65,102 +65,6 @@ class BlipCaptionProcessor(BaseProcessor):
             caption = " ".join(caption_words[: self.max_words])
 
         return caption
-
-
-# @registry.register_processor("blip_question")
-# class BlipQuestionProcessor(BaseProcessor):
-#     def __init__(self, max_words=50):
-#         self.max_words = max_words
-#
-#     def __call__(self, question):
-#         return self.pre_question(question)
-#
-#     @classmethod
-#     def from_config(cls, cfg=None):
-#         if cfg is None:
-#             cfg = OmegaConf.create()
-#
-#         max_words = cfg.get("max_words", 50)
-#
-#         return cls(max_words=max_words)
-#
-#     def pre_question(self, question):
-#         question = re.sub(
-#             r"([.!\"()*#:;~])",
-#             "",
-#             question.lower(),
-#         )
-#         question = question.rstrip(" ")
-#
-#         # truncate question
-#         question_words = question.split(" ")
-#         if len(question_words) > self.max_words:
-#             question = " ".join(question_words[: self.max_words])
-#
-#         return question
-
-
-# @registry.register_processor("blip_image_train")
-# class BlipImageTrainProcessor(BlipImageBaseProcessor):
-#     def __init__(
-#             self, image_size=384, mean=None, std=None, min_scale=0.5, max_scale=1.0
-#     ):
-#         super().__init__(mean=mean, std=std)
-#
-#         self.transform = transforms.Compose(
-#             [
-#                 transforms.RandomResizedCrop(
-#                     image_size,
-#                     scale=(min_scale, max_scale),
-#                     interpolation=InterpolationMode.BICUBIC,
-#                 ),
-#                 transforms.RandomHorizontalFlip(),
-#                 RandomAugment(
-#                     2,
-#                     5,
-#                     isPIL=True,
-#                     augs=[
-#                         "Identity",
-#                         "AutoContrast",
-#                         "Brightness",
-#                         "Sharpness",
-#                         "Equalize",
-#                         "ShearX",
-#                         "ShearY",
-#                         "TranslateX",
-#                         "TranslateY",
-#                         "Rotate",
-#                     ],
-#                 ),
-#                 transforms.ToTensor(),
-#                 self.normalize,
-#             ]
-#         )
-#
-#     def __call__(self, item):
-#         return self.transform(item)
-#
-#     @classmethod
-#     def from_config(cls, cfg=None):
-#         if cfg is None:
-#             cfg = OmegaConf.create()
-#
-#         image_size = cfg.get("image_size", 384)
-#
-#         mean = cfg.get("mean", None)
-#         std = cfg.get("std", None)
-#
-#         min_scale = cfg.get("min_scale", 0.5)
-#         max_scale = cfg.get("max_scale", 1.0)
-#
-#         return cls(
-#             image_size=image_size,
-#             mean=mean,
-#             std=std,
-#             min_scale=min_scale,
-#             max_scale=max_scale,
-#         )
-
 
 # @registry.register_processor("blip_image_eval")
 class BlipImageEvalProcessor(BlipImageBaseProcessor):
